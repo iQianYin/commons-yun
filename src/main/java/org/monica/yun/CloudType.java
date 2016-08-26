4a47352e72ed7027f8e51bfdcbf5e4133c91147d
iQianYin <jinqianyin@126.com>
2016-08-26 17:23:52 +0800

    Enum CloudType

---
+++ src/main/java/org/monica/yun/CloudType.java
@@ -0,0 +1,26 @@
+package org.monica.yun;
+
+public enum CloudType {
+
+	/** 0: 本地文件local **/
+	LOCAL("local file storage"),
+
+	/** 1: 亚马逊s3 **/
+	S3("amazon s3 storage"),
+
+	/** 2: 阿里巴巴oss **/
+	OSS("alibaba oss storage"),
+
+	/** 3: 金山云ks3 **/
+	KS3("kingsoft ks3 storage");
+
+	private String type;
+
+	private CloudType(String type) {
+		this.type = type;
+	}
+
+	public String toString() {
+		return this.type;
+	}
+}
